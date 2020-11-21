e9ad22b12c236c035c2fc436208090e7572c4365
Lars Karbo <mail@larskarbo.no>
2020-11-21 12:24:25 +0100

    og

--- App.tsx
+++ App.tsx
@@ -18,6 +18,7 @@ import LoginPage from "./src/views/LoginPage";
 import IntroPage from "./src/views/IntroPage";
 import { request, generateHeaders } from "./src/utils/request";
 import Croaker from "./src/Croaker";
+import { Helmet } from "react-helmet";
 import { BButton } from "./src/comp/BButton";
 
 // You must run this once before trying to interact with the widget
@@ -71,6 +72,20 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Helmet>
+        <meta
+          name="description"
+          content="Organize and annotate songs and segments from Spotify, Youtube, Soundcloud in interactive, shareable documents"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dfzqjzusj/image/upload/c_fill,g_north,h_630,w_1200/v1605177986/CleanShot_2020-11-12_at_11.45.29_2x.png"
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/dfzqjzusj/image/upload/c_fill,g_north,h_630,w_1200/v1605177986/CleanShot_2020-11-12_at_11.45.29_2x.png"
+        />
+      </Helmet>
       <Router>
         <Switch>
           <Route exact path="/">
@@ -80,7 +95,7 @@ export default function App() {
           <Route exact path="/login">
             <LoginPage user={user} />
           </Route>
-          <Route path={["/s/profile","/s/browse", "/s/:collectionId", "/s"]}>
+          <Route path={["/s/profile", "/s/browse", "/s/:collectionId", "/s"]}>
             <Croaker loadingUser={loadingUser} user={user} />
           </Route>
           {/* <Switch>
