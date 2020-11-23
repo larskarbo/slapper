878b86441858d835d1afedaf571480663ca501bb
Lars Karbo <mail@larskarbo.no>
2020-11-23 08:51:38 +0100

    fix setMe endpoint

--- src/views/OnBoard.tsx
+++ src/views/OnBoard.tsx
@@ -37,7 +37,7 @@ export default function ({ user, register = false }) {
     }
     // const password = formRef.current.password.value;
 
-    request("POST", "/fauna/users/setMe", {
+    request("POST", "sfauna/users/setMe", {
       username:username,
       emailUpdates:emailUpdates,
     })
